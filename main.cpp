09590233cc29489339e185c9407e00131576c356
Ty <tysanders@tamu.edu>
2024-05-14 14:20:30 -0500

    correctly reading table info

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fmt/core.h>
 #include <arpa/inet.h>
+#include <inttypes.h>
 
 using namespace std;
 
@@ -14,6 +15,43 @@ struct TableDirectory {
     uint16_t entrySelector;
     uint16_t rangeShift;
 };
+
+
+//TABLE RECORDS
+struct TableRecord {
+    uint32_t tag;
+    uint32_t checkSum;
+    uint32_t offset;  // offset TYPE
+    uint32_t length;
+};
+
+struct TableRecords {
+    vector<TableRecord> records;
+};
+
+
+//CMAP Specific
+struct encodingRecord {
+    uint16_t platformID;
+    uint16_t encodingID;
+    uint32_t offset; // offset TYPE
+};
+
+struct cmapHeader {
+    uint16_t version;
+    uint16_t numTables;
+    vector<encodingRecord> encodingRecords;
+};
+
+
+
+
+// hex to ascii printout
+void hex_to_ascii(uint32_t hex) {
+    char str[11];
+    sprintf(str, "%c%c%c%c", hex >> 24, hex >> 16, hex >> 8, hex);
+    fmt::print("Tag: {}\n", str);
+}
 
 // Function to read a 32-bit unsigned integer from the file
 uint32_t readUint32(ifstream& file) {
@@ -40,6 +78,39 @@ TableDirectory readTableDirectory(ifstream& file) {
     return dir;
 }
 
+TableRecords readTableRecords(ifstream& file, const TableDirectory& dir) {
+    TableRecords records;
+    //seek forward size of TableDirectory
+    file.seekg(sizeof(TableDirectory));
+
+    for (int i = 0; i < dir.numTables; i++) {
+        TableRecord record;
+        record.tag = readUint32(file);
+        record.checkSum = readUint32(file);
+        record.offset = readUint32(file);
+        record.length = readUint32(file);
+        //push into vector
+        records.records.push_back(record);
+    }
+    return records;
+}
+
+cmapHeader readCmapHeader(ifstream& file, uint32_t offset) {
+    file.seekg(0); //offset from beginning of file
+    file.seekg(offset);
+    cmapHeader header;
+    header.version = readUint16(file);
+    header.numTables = readUint16(file);
+    for (int i = 0; i < header.numTables; i++) {
+        encodingRecord record;
+        record.platformID = readUint16(file);
+        record.encodingID = readUint16(file);
+        record.offset = readUint32(file);
+        header.encodingRecords.push_back(record);
+    }
+    return header;
+}
+
 // Function to print the first 256 bytes of the file as hexadecimal
 void printFile256(ifstream& file) {
     file.seekg(0); // Seek to the beginning of the file
@@ -59,8 +130,20 @@ void printTableDirectory(const TableDirectory& dir) {
     fmt::print("Range shift: {}\n", dir.rangeShift);
 }
 
+void printTableRecords(const TableRecords& records) {
+    for (const auto& record : records.records) {
+        fmt::print("-------------------------\n");
+        hex_to_ascii(record.tag);
+        fmt::print("tag hex: {:#x}\n", record.tag);
+        fmt::print("CheckSum: {:#x}\n", record.checkSum);
+        fmt::print("Offset: {:#x}\n", record.offset);
+        fmt::print("Length: {:#x}\n", record.length);
+    }
+    fmt::print("-------------------------\n");
+}
+
 int main() {
-    ifstream file("test.otf", ios::binary);
+    ifstream file("Helvetica.otf", ios::binary);
     if (!file.is_open()) {
         cerr << "Error opening file!" << endl;
         return 1;
@@ -68,13 +151,37 @@ int main() {
 
     // Read the table directory
     TableDirectory dir = readTableDirectory(file);
+    TableRecords records = readTableRecords(file, dir);
 
     // Print the SFNT version and the number of tables
     printTableDirectory(dir);
+    printTableRecords(records);
 
-    // Print the first 256 bytes of the file as hexadecimal
-    printFile256(file);
+    //get cmap
+    //get record entry for cmap
+    TableRecord cmapRecord;
+    //find relevant entry
+    for (const auto& record : records.records) {
+        if (record.tag == 0x636d6170) {
+            cmapRecord = record;
+            break;
+        }
+    }
+
+    cmapHeader cmapHeader = readCmapHeader(file, cmapRecord.offset);
+    cout << "Cmap Header: " << cmapHeader.version << endl;
+    cout << "Cmap Header: " << cmapHeader.numTables << endl;
+    for (int i = 0; i < cmapHeader.numTables; i++) {
+        fmt::print("-------------------------\n");  
+        cout << "Platform ID: " << cmapHeader.encodingRecords[i].platformID << endl;
+        cout << "Encoding ID: " << cmapHeader.encodingRecords[i].encodingID << endl;
+        cout << "Offset: " << cmapHeader.encodingRecords[i].offset << endl;
+    }
 
     file.close();
     return 0;
 }
+
+
+
+// w? {hs* vs* cm* hm* mt subpath}? {mt subpath}* endchar
