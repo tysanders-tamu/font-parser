7d5786caa9b8d0f82aa0be396722ef522cf635f0
MikeNix12 <mtnix12@tamu.edu>
2024-05-20 15:49:13 -0500

    Functions for parsing charstrings added

--- src/CharString.cpp
+++ src/CharString.cpp
@@ -12,37 +12,67 @@ void CharString::addHint(int x, int y, bool isV){
   }
 }
 
-int CharString::getNextInt(){
-  int result = 0;
-  while(!values.empty()){
-    uint8_t val = values.front();
+int32_t CharString::getNextNum(){
+  int32_t result = 0;
+  uint8_t val = values.front();
+  if (val = 28){
     values.pop();
-    if (val = 28){
-      result = values.front();
-      values.pop();
-      result = (result << 8) | values.front();
-      values.pop();
-    }
-    else if(val < 32){
-      operators.push_back(val);
-      return getNextInt();
-    }else if (val < 247){
-      result = val - 139;
-    } else if (val < 251){
-      result = (result << 8) | (((val - 247) << 8) | values.front());
-      values.pop();
-    } else if (val < 255){
-      result = (result << 8) | (-(val - 251) << 8) | values.front();
-      values.pop();
-    } else{
-      result = (result << 8) | (values.front() << 8) | values.front();
-      values.pop();
-      values.pop();
-    }
-    if(val < 32){
-      break;
-    }
+    result = values.front();
+    values.pop();
+    result = (result << 8) | values.front();
+    values.pop();
   }
+  else if(val < 32){
+    throw("NaN - Reserved Operator");
+  }else if (val < 247){
+    values.pop();
+    result = val - 139;
+  } else if (val < 251){
+    values.pop();
+    result = (((val - 247) << 8) + values.front() + 108);
+    values.pop();
+  } else if (val < 255){
+    values.pop();
+    result = -((val - 251) << 8) - values.front()  - 108;
+    values.pop();
+  } else{
+    values.pop();
+    result = (val << 24) | (values.front() << 16);
+    values.pop();
+    result = (result | (values.front() << 8));
+    values.pop();
+    result = (result | values.front());
+    values.pop();
+  }   
   return result;
 }
 
+void CharString::getValsQueue(const std::vector<uint8_t> &vals){
+  for (auto val : vals){
+    values.push(val);
+  }
+}
+
+void CharString::parseVals(){
+  while (!values.empty()){
+    uint16_t val = 0x0000 | values.front();
+    if (val == 12){
+      values.pop();
+      val = values.front();
+      values.pop();
+      std::string op = two_byte_operators[val];
+      if(op != ""){
+        opers.push(op);
+      } else {
+        throw("Reserved Operator");
+      }
+    } 
+    else if (val < 32 && val != 28){
+      opers.push(one_byte_operators[val]);
+      values.pop();
+    } 
+    else {
+      nums.push(getNextNum());
+    }
+  }
+}
--- src/CharString.hpp
+++ src/CharString.hpp
@@ -3,25 +3,119 @@
 #include <memory>
 #include <queue>
 #include <vector>
+#include <string>
 #include <utility>
+
+
+struct hint{
+  int x;
+  int y;
+  bool isV;
+};
+
+struct point{
+  int x;
+  int y;
+};
+
+
+const std::string one_byte_operators[] = {
+  "",
+  "hstem",
+  "",
+  "vstem",
+  "vmoveto",
+  "rlineto",
+  "hlineto",
+  "vlineto",
+  "rrcurveto",
+  "",
+  "callsubr",
+  "return",
+  "escape",
+  "",
+  "endchar",
+  "",
+  "",
+  "",
+  "hstemhm",
+  "hintmask",
+  "cntrmask",
+  "rmoveto",
+  "hmoveto",
+  "vstemhm",
+  "rcurveline",
+  "rlinecurve",
+  "vvcurveto",
+  "hhcurveto",
+  "shortint",
+  "callgsubr",
+  "vhcurveto",
+  "hvcurveto"
+};
+
+const std::string two_byte_operators[] = {
+  "",
+  "",
+  "",
+  "and",
+  "or",
+  "not",
+  "",
+  "",
+  "",
+  "abs",
+  "add",
+  "sub",
+  "div",
+  "",
+  "neg",
+  "eq",
+  "",
+  "",
+  "drop",
+  "",
+  "put",
+  "get",
+  "ifelse",
+  "random",
+  "mul",
+  "",
+  "sqrt",
+  "dup",
+  "exch",
+  "index",
+  "roll",
+  "",
+  "",
+  "",
+  "hflex",
+  "flex",
+  "hflex1",
+  "flex1",
+  ""
+};
 
 class CharString{
   CharString():x_pos(0),y_pos(0), defaultWidthX(256) {};
   ~CharString(){};
 
   std::queue<uint8_t> values;
-  std::vector<std::pair<int,int>> points;
-  std::vector<std::pair<int,int>> h_hints;
-  std::vector<std::pair<int,int>> v_hints;
+  std::vector<point> points;
+  std::vector<hint> h_hints;
+  std::vector<hint> v_hints;
   std::vector<bool> hintmasks;
-  std::vector<uint8_t> operators;
+  std::queue<std::string> opers;
+  std::queue<int32_t> nums;
   int defaultWidthX;
   int x_pos;
   int y_pos;  
 
   void addPoint(int x, int y);
   void addHint(int x, int y, bool isV);
-  int getNextInt();
+  int32_t getNextNum();
+  void getValsQueue(const std::vector<uint8_t> &vals);
+  void parseVals();
 };
 
-#endif 
+#endif
